ff2410778c3a518346c98fa0b9768699763948f3
Bui Hoang Huy <huy.buihoang2301@gmail.com>
2024-04-10 11:40:58 +0700

    delete redundant

--- lib/pages/authenticate_pages/sign_in.dart
+++ lib/pages/authenticate_pages/sign_in.dart
@@ -1,5 +1,3 @@
-import 'dart:isolate';
-
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
