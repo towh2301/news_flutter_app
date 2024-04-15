f831883c975e6de9c210b32d7de8c31d9579d1a8
Bui Hoang Huy <huy.buihoang2301@gmail.com>
2024-04-15 14:56:21 +0700

    update list

--- lib/common/rss_list.dart
+++ lib/common/rss_list.dart
@@ -1,5 +1,5 @@
 const Map<String, String> baonhandanRssList = {
-  //'Trang chủ': 'https://nhandan.vn/rss/home.rss',
+  'Trang chủ': 'https://nhandan.vn/rss/home.rss',
   'Chính trị': 'https://nhandan.vn/rss/chinhtri-1171.rss',
   'Xã luận': 'https://nhandan.vn/rss/xa-luan-1176.rss',
   'Bình luận': 'https://nhandan.vn/rss/binh-luan-phe-phan-1180.rss',
@@ -10,6 +10,21 @@ const Map<String, String> baonhandanRssList = {
   'Thế giới': 'https://nhandan.vn/rss/thegioi-1231.rss',
   'KH - CN': 'https://nhandan.vn/rss/khoahoc-congnghe-1292.rss'
 };
+
+const Map<String, String> congannhandanRssList = {
+  'Trang chủ': 'https://cadn.com.vn/tin-moi-nhat.rss',
+  'Thời sự': 'https://cadn.com.vn/rss/thoi-su-1.rss',
+  'Công an Nhân dân': 'https://cadn.com.vn/rss/cong-an-nhan-dan-5.rss',
+  'Pháp luật': 'https://cadn.com.vn/rss/phap-luat-6.rss',
+  'Phóng sự - Ký sự': 'https://cadn.com.vn/rss/phong-su-ky-su-79.rss',
+  'An ninh - Trật tự': 'https://cadn.com.vn/rss/an-ninh-trat-tu-78.rss',
+  'Kinh tế': 'https://cadn.com.vn/rss/kinh-te-12.rss',
+  'Văn hoá': 'https://cadn.com.vn/rss/van-hoa-15.rss',
+  'Xã hội': 'https://cadn.com.vn/rss/xa-hoi-13.rss',
+  'Y tế': 'https://cadn.com.vn/rss/y-te-68.rss',
+  'Công nghệ số': 'https://cadn.com.vn/rss/cong-nghe-so-116.rss'
+};
+
 const Map<String, String> vnexpressRssList = {
   'Trang chủ': 'https://vnexpress.net/rss/tin-moi-nhat.rss',
   'Thời sự': 'https://vnexpress.net/rss/thoi-su.rss',
@@ -27,12 +42,14 @@ class RssMapFile {
   List<Map<String, String>> rssMap = [
     vnexpressRssList,
     baonhandanRssList,
+    congannhandanRssList,
   ];
 }
 
 class WebsitesList {
-  List<Map<String, String>> websList = [
-    vnexpressRssList,
-    baonhandanRssList,
-  ];
+  Map<String, Map> websList = {
+    "Báo Nhân Dân": baonhandanRssList,
+    "VNExpress": vnexpressRssList,
+    "Công an Nhân dân": congannhandanRssList,
+  };
 }
