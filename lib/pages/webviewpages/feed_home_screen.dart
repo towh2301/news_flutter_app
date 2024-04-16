d48ecd78380033014190f300e38ee75cc9f3f6f1
Bui Hoang Huy <huy.buihoang2301@gmail.com>
2024-04-16 16:09:48 +0700

    modify some comments

--- lib/pages/webviewpages/feed_home_screen.dart
+++ lib/pages/webviewpages/feed_home_screen.dart
@@ -43,9 +43,6 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Drawer sideBarCustom(context, Map websiteMap) {
     return Drawer(
-      // Add a ListView to the drawer. This ensures the user can scroll
-      // through the options in the drawer if there isn't enough vertical
-      // space to fit everything.
       child: ListView(
         // Important: Remove any padding from the ListView.
         padding: EdgeInsets.zero,
